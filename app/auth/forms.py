29dda2a097e85028ad11d3917191863b0c42835f
Abbott Wu <wu13231423396@outlook.com>
2017-08-06 14:52:36 +0800

    complete auth unit (withou unit test)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from .. models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +11,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('密码', validators=[Required()])
     remember_me = BooleanField()
     submit = SubmitField('登录')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email')
+    username = StringField('Username')
+    password = PasswordField('password')
+    password2 = PasswordField('confirm password')
+    submit = SubmitField('Register')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, session
 from flask_login import current_user, login_user, login_required, logout_user
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
+from ..email import send_email
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -16,9 +18,75 @@ def login():
         flash('密码或用户名错误')
     return render_template('auth/login.html.j2', current_user=current_user, form=form)
 
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('你已经退出')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password2.data:
+            flash('密码不相等，请重新输入')
+            return redirect(url_for('auth.register'))
+        elif User.query.filter_by(email=form.email.data).first():
+            flash('邮箱已注册，请重新输入')
+            return redirect(url_for('auth.register'))
+        elif User.query.filter_by(username=form.username.data).first():
+            flash('用户名已注册，请重新输入')
+            return redirect(url_for('auth.register'))
+        else:
+            user = User(email=form.email.data,
+                        username=form.username.data,
+                        password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            token = user.generate_confirmation_token()
+            send_email(user.email, '确认你的账户', 'auth/email/confirm',
+                       user=user, token=token)
+            flash('你现在可以登录了，有一封邮件已经邮寄到你的邮箱了')
+            return redirect(url_for('auth.login'))
+    return render_template('auth/register.html.j2', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('你已经确认你的账户')
+    else:
+        flash('确认失败')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html.j2')
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, '确认你的账户',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('一份新的邮件已经被送到你的邮箱里了')
     return redirect(url_for('main.index'))
